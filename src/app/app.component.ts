4663d20490314fe2be52b483584f995688011a57
BMB0 <bruno.marquez@ucb.edu.bo>
2025-02-05 22:37:49 -0400

    feat: add task list component and update routing configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet  } from '@angular/router';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, TaskListComponent],
+  template: `<router-outlet></router-outlet>`,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'task-evaluation-front';
