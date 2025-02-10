0e281846c4acc3b269dbc8a26859ee61cb9dad7e
Jose Carlos Lozada Peralta <josecarlosyoung74@gmail.com>
2025-02-09 23:41:45 -0400

    Added a loading and an error state to improve the user experience

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -13,16 +13,25 @@ export class TasksComponent {
   http_client = inject(HttpClient); 
   tasks: any[] = [];
   API_URL = 'http://localhost:3000/tasks';
+  loading: boolean = false;
+  error: boolean = false;
 
   constructor() {
     this.fetchTasks();
   }
 
   async fetchTasks() {
+    this.loading = true;
+    console.log('Fetching tasks');
     this.http_client.get<any[]>(this.API_URL).subscribe({
       next: (data) => {
         this.tasks = data;
+      },
+      error: (error) => {
+        console.error('Error fetching tasks', error);
+        this.error = true;
       }
     });
+    this.loading = false;
   }
 }
